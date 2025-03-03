94a9a9c51ef061dd3ea56ffdb39ff85c7dbc8402
arzakaraffan <arzakaraffan@gmail.com>
2025-03-04 02:27:56 +0700

    [GREEN] Add tests and Implement Payment Model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -2,15 +2,18 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
 import java.util.Map;
+import java.util.UUID;
 
 @Getter
 public class Payment {
+    String id;
     Order order;
     String method;
     Map<String, String> paymentData;
     String status = "PENDING";
 
     public Payment(Order order, String method, Map<String, String> paymentData) {
+        this.id = UUID.randomUUID().toString();
         this.method = method;
         this.paymentData = paymentData;
         if (order == null) {
